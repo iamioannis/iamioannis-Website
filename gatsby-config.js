733a3125cfe407c73bd36f0b47320e274e51dc7b
Ioannis Mastigopoulos <johnmasti@gmail.com>
2021-02-22 19:40:50 +0200

    Part 8: Added Projects page with Json Data

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/data`,
+      },
+    },
     {
       resolve: `gatsby-transformer-remark`,
       options: {
