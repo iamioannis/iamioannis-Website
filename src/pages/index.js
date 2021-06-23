06d409b0805e806ab494062827f7e302eaf88fc8
Ioannis <dev@iamioannis.com>
2021-06-23 14:33:31 +0300

    Added logo image to home

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,31 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { StaticImage } from "gatsby-plugin-image"
 
 
-const IndexPage = () => (
+
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <div style={{ maxWidth: `400px`, marginBottom: `1.45rem` }}>
-    <h1 className="index_title">Ioannis Mastigopoulos</h1>
-    <p>👋 Hi</p>
-    <p>Welcome to my personal Internet corner</p>
-    <Link to="/blog/">📝 Writing</Link> <br/>
-    <Link to="/about">🙋‍♂️ About me</Link> <br/>
-    <Link to="/ch">👋 Clubhouse notes</Link> <br/>
+      <StaticImage src="../images/yiannisfinalsmallerst.png" 
+      alt="iamioannis Homepage"
+      layout="fixed"
+      height={100}
+      imgStyle={{ objectFit: 'contain' }}
+      />
+      <h1 className="index_title">Ioannis Mastigopoulos</h1>
+      <p>👋 Hi</p>
+      <p>Welcome to my personal Internet corner</p>
+      <Link to="/blog/">📝 Writing</Link> <br/>
+      <Link to="/about">🙋‍♂️ About me</Link> <br/>
+      <Link to="/ch">👋 Clubhouse notes</Link> <br/>
     </div>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
+
+
