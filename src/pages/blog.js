122de7ebc91b55197403dd40938b722a49c6f7e4
Ioannis <dev@iamioannis.com>
2021-08-02 03:17:39 +0300

    Fix website static content Fixes #30

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -15,11 +15,11 @@ const BlogPage = ({ data }) => {
       <h1 className={"post-header"}>Writing</h1>
       <Navigation></Navigation>
       <div className="post-list">
-        <p>Welcome to my Writing collateral. <br/>The content included is a collection of all my longform and short form writing that might be spread around mediums. Taking the notion of making this website as my personal corner of the internet, I want to make sure I collect most of the content I produce. Some of the categories of content are described below</p>
+        <p>Welcome to my Writing collateral. <br/>The content included is a collection of all my longform and short form writing that might be spread around mediums. Taking the notion of making this website my personal corner of the internet, I want to make sure I collect most of the content I produce. Some of the categories of content are described below</p>
         <h2>Blog</h2>
         <p>The "Blog" is my reference of writing through time. It can be longform or short form. All writing is collected at the Archive below. When (and if) the content list gets long you can us <Link to="/tags">Tags</Link> to find what topic you are interested easier</p>
         <h2>Newsletter</h2>
-        <p>Periodically I'm (about) to forward some of my writing throught my Newsletter. The format will can be long or short collections of many items. What is definitely going to be about topics that I find interestings and I hope you find them too. To receive issues of my newsletter you can use the subscibe box below or read more about it in the <Link to="/newsletter/">Newsletter Page</Link></p>
+        <p>Periodically I'm (about) to forward some of my writing throught my Newsletter. The format can be long or short or collections of items. Topics I want to write about or interesting content from around the web. To receive this periodiacal you can use the subscibe box below to get into my mailing list or read more about it in the <Link to="/newsletter/">Newsletter Page</Link></p>
         <h2>Archive</h2>
         {posts.map(post => (
           <ul key={post.node.id} className="post-list__item">
